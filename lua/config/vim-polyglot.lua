3f4d629cf6eddf6a32c1c37904ee5f5ffef60c43
Tai transytai12a1 <transytai12a1@gmail.com>
2024-06-18 08:03:27 +0700

    fix: set disable syntax highlight

--- lua/config/vim-polyglot.lua
+++
@@ -1,2 +0,0 @@
--- Disable vim-polyglot for specific languages to avoid conflicts with nvim-treesitter
-vim.g.polyglot_disabled = { "regex", "bash", "css", "html", "javascript", "json", "lua", "python", "ruby", "typescript", "yaml" }
