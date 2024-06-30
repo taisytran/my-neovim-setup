47b81e58bf5f387f7b72e09c7c6e548cbe68dc5e
Tai Tran <transytai12a1@gmail.com>
2024-06-30 08:51:36 +0700

    improve: disable treesitter and update some plugins

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -1,11 +1,11 @@
 -- config/gitsigns.lua
 require('gitsigns').setup {
   signs = {
-    add          = {hl = 'GitGutterAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitGutterChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitGutterDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitGutterDeleteChange', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitGutterChangeDelete', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add          = { text = '│' },
+    change       = { text = '│' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
   },
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
@@ -29,11 +29,26 @@ require('gitsigns').setup {
     virt_text_pos = 'eol',
     delay = 1000,
   },
-  current_line_blame_formatter_opts = {
-    relative_time = false
-  },
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil,
   word_diff = false,
 }
+
+-- Define highlights for GitSigns
+vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GitGutterAdd' })
+vim.api.nvim_set_hl(0, 'GitSignsAddNr', { link = 'GitGutterAdd' })
+vim.api.nvim_set_hl(0, 'GitSignsAddLn', { link = 'GitGutterAdd' })
+vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'GitGutterChange' })
+vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { link = 'GitGutterChange' })
+vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { link = 'GitGutterChange' })
+vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GitGutterDelete' })
+vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { link = 'GitGutterDelete' })
+vim.api.nvim_set_hl(0, 'GitSignsDeleteLn', { link = 'GitGutterDelete' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { link = 'GitGutterDeleteChange' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { link = 'GitGutterDeleteChange' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdeleteLn', { link = 'GitGutterDeleteChange' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { link = 'GitGutterChangeDelete' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedeleteNr', { link = 'GitGutterChangeDelete' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedeleteLn', { link = 'GitGutterChangeDelete' })
--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "regex", "bash", "css", "html", "javascript", "json", "lua", "python", "ruby", "typescript", "yaml", "svelte" },
+  ensure_installed = { "regex", "bash", "css", "html", "javascript", "json", "lua", "python", "ruby", "typescript", "yaml" },
   sync_install = false,
   -- ignore_install = { "typescript" },
 
