e751c316cb48a401de113fe6127237f943b8d9b1
Tai Tran <transytai12a1@gmail.com>
2025-12-04 10:55:20 +0700

    chore(ktlo): monthly dependency maintenance

--- init.lua
+++ init.lua
@@ -2,168 +2,168 @@
 vim.opt.runtimepath:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
 require("lazy").setup({
-  -- Core dependencies
-  "nvim-lua/plenary.nvim",
+	-- Core dependencies
+	"nvim-lua/plenary.nvim",
 
-  -- Impatient plugin to improve startup time
-  "lewis6991/impatient.nvim",
+	-- Impatient plugin to improve startup time
+	"lewis6991/impatient.nvim",
 
-  -- 'kylechui/nvim-surround', -- Surround text objects easily
-  -- File Explorer
-  {
-    "kyazdani42/nvim-tree.lua", -- File system explorer
-    cmd = { "NvimTreeToggle", "NvimTreeFindFile" },
-    dependencies = "kyazdani42/nvim-web-devicons",
-  },
+	-- 'kylechui/nvim-surround', -- Surround text objects easily
+	-- File Explorer
+	{
+		"kyazdani42/nvim-tree.lua", -- File system explorer
+		cmd = { "NvimTreeToggle", "NvimTreeFindFile" },
+		dependencies = "kyazdani42/nvim-web-devicons",
+	},
 
-  -- Fuzzy Finder
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    dependencies = "nvim-telescope/telescope.nvim",
-  },
+	-- Fuzzy Finder
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+		dependencies = "nvim-telescope/telescope.nvim",
+	},
 
-  -- {
-  --   'pbogut/fzf-mru.vim',
-  --   requires = { 'nvim-telescope/telescope-fzf-native.nvim' }
-  -- },
+	-- {
+	--   'pbogut/fzf-mru.vim',
+	--   requires = { 'nvim-telescope/telescope-fzf-native.nvim' }
+	-- },
 
-  -- Statusline
-  {
-    "hoob3rt/lualine.nvim",
-    event = "VeryLazy",
-  },
-  -- 'lewis6991/gitsigns.nvim', -- Git diff in sign column
+	-- Statusline
+	{
+		"hoob3rt/lualine.nvim",
+		event = "VeryLazy",
+	},
+	-- 'lewis6991/gitsigns.nvim', -- Git diff in sign column
 
-  -- Git integration
-  {
-    "tpope/vim-fugitive",
-    cmd = { "Git", "G" },
-  },
+	-- Git integration
+	{
+		"tpope/vim-fugitive",
+		cmd = { "Git", "G" },
+	},
 
-  {
-    "lewis6991/gitsigns.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 
-  "numToStr/Comment.nvim", -- Commenting utility
+	"numToStr/Comment.nvim", -- Commenting utility
 
-  -- Syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufRead", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-      "windwp/nvim-ts-autotag",
-    },
-    enabled = true,
-  },
-  -- {
-  --   'sheerun/vim-polyglot',
-  --   init = function ()
-  --     -- TODO: not working with treesitter
-  --     -- vim.g.polyglot_disabled = { "regex", "bash", "css", "html", "javascript", "json", "lua", "python", "ruby", "typescript", "yaml" }
-  --   end
-  -- },
-  -- Navigation - EasyMotion
-  {
-    "phaazon/hop.nvim",
-    event = "VeryLazy",
-  },
-  -- Nord color scheme
-  "shaunsingh/nord.nvim",
+	-- Syntax highlighting
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufRead", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+		},
+		enabled = true,
+	},
+	-- {
+	--   'sheerun/vim-polyglot',
+	--   init = function ()
+	--     -- TODO: not working with treesitter
+	--     -- vim.g.polyglot_disabled = { "regex", "bash", "css", "html", "javascript", "json", "lua", "python", "ruby", "typescript", "yaml" }
+	--   end
+	-- },
+	-- Navigation - EasyMotion
+	{
+		"phaazon/hop.nvim",
+		event = "VeryLazy",
+	},
+	-- Nord color scheme
+	"shaunsingh/nord.nvim",
 
-  -- Indentation
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufRead",
-  },
+	-- Indentation
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "BufRead",
+	},
 
-  -- LSP Configuration
-  {
-    "mason-org/mason.nvim",
-    -- { "mason-org/mason-lspconfig.nvim", version = "v1.32.0" },
-    "mason-org/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  },
-  -- Formatter
-  {
-    -- archived
-    -- 'jose-elias-alvarez/null-ls.nvim'
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-  },
-  -- 'glepnir/lspsaga.nvim',
-  -- Autocompletion
-  {
-    "hrsh7th/nvim-cmp",
-    commit = "b356f2c", --TODO: https://github.com/hrsh7th/nvim-cmp/issues/1877
-    pin = true,
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/vim-vsnip",
-      "hrsh7th/cmp-vsnip",
-      "rafamadriz/friendly-snippets",
-      "onsails/lspkind-nvim",
-    },
-  },
+	-- LSP Configuration
+	{
+		"mason-org/mason.nvim",
+		-- { "mason-org/mason-lspconfig.nvim", version = "v1.32.0" },
+		"mason-org/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	-- Formatter
+	{
+		-- archived
+		-- 'jose-elias-alvarez/null-ls.nvim'
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+	},
+	-- 'glepnir/lspsaga.nvim',
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		commit = "b356f2c", --TODO: https://github.com/hrsh7th/nvim-cmp/issues/1877
+		pin = true,
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/vim-vsnip",
+			"hrsh7th/cmp-vsnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind-nvim",
+		},
+	},
 
-  -- modify multiple lines
-  {
-    "mg979/vim-visual-multi",
-    event = "BufRead", -- load when open file
-  },
+	-- modify multiple lines
+	{
+		"mg979/vim-visual-multi",
+		event = "BufRead", -- load when open file
+	},
 
-  -- notice
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-      -- {
-      -- "rcarriga/nvim-notify",
-      -- commit = "a5af69dc4866e85f2aeeccf309444eb811bb96cc",
-      -- },
-    },
-  },
-  {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
-  },
-  {
-    "szw/vim-maximizer",
-    event = { "BufRead", "BufNewFile" },
-  },
+	-- notice
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+			-- {
+			-- "rcarriga/nvim-notify",
+			-- commit = "a5af69dc4866e85f2aeeccf309444eb811bb96cc",
+			-- },
+		},
+	},
+	{
+		"sindrets/diffview.nvim",
+		cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+	},
+	{
+		"szw/vim-maximizer",
+		event = { "BufRead", "BufNewFile" },
+	},
 
-  -- startup
-  {
-    "goolord/alpha-nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
+	-- startup
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 
-  -- marks
-  -- {
-  --   "chentoast/marks.nvim",
-  -- },
+	-- marks
+	-- {
+	--   "chentoast/marks.nvim",
+	-- },
 
-  -- tab
-  {
-    "alvarosevilla95/luatab.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("luatab").setup({})
-    end,
-  },
-  -- {
-  --   'akinsho/bufferline.nvim',
-  -- }
+	-- tab
+	{
+		"alvarosevilla95/luatab.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("luatab").setup({})
+		end,
+	},
+	-- {
+	--   'akinsho/bufferline.nvim',
+	-- }
 })
 
 require("config.basic")
--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -1,47 +1,58 @@
 local conform = require("conform")
 
+-- health check :ConformInfo
+
 conform.setup({
-  formatters_by_ft = {
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescriptreact = { "prettier" },
-    json = { "prettier" },
-    html = { "prettier" },
-    css = { "prettier" },
-    yaml = { "prettier" },
-    markdown = { "prettier" },
-    python = { "black" },
-    lua = { "stylua" },
-  },
-  format_on_save = {
-    lsp_fallback = true,
-    timeout_ms = 1000,
-  },
-  notify_on_error = true,
+	formatters_by_ft = {
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		json = { "prettier" },
+		html = { "prettier" },
+		css = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		python = { "black" },
+		lua = { "stylua" },
+		sql = { "pg_format" },
+		pgsql = { "pg_format" },
+	},
+	formatters = {
+		pg_format = {
+			command = "pg_format",
+			stdin = true,
+			args = { "-L", "-s", "2", "-u", "2", "-" },
+		},
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		timeout_ms = 1000,
+	},
+	notify_on_error = true,
 })
 
 vim.keymap.set("n", "<leader>=", function()
-  local line = vim.api.nvim_win_get_cursor(0)[1]
-  require("conform").format({
-    async = true,
-    lsp_fallback = true,
-    range = {
-      start = { line, 0 },
-      ["end"] = { line + 1, 0 },
-    },
-  })
+	local line = vim.api.nvim_win_get_cursor(0)[1]
+	require("conform").format({
+		async = true,
+		lsp_fallback = true,
+		range = {
+			start = { line, 0 },
+			["end"] = { line + 1, 0 },
+		},
+	})
 end, { desc = "Format current line with Conform" })
 
 vim.keymap.set("v", "<leader>=", function()
-  local start_pos = vim.fn.getpos("'<")
-  local end_pos = vim.fn.getpos("'>")
-  require("conform").format({
-    async = true,
-    lsp_fallback = true,
-    range = {
-      start = { start_pos[2], start_pos[3] },
-      ["end"] = { end_pos[2] + 1, 0 }, -- +1 vì end exclusive
-    },
-  })
+	local start_pos = vim.fn.getpos("'<")
+	local end_pos = vim.fn.getpos("'>")
+	require("conform").format({
+		async = true,
+		lsp_fallback = true,
+		range = {
+			start = { start_pos[2], start_pos[3] },
+			["end"] = { end_pos[2] + 1, 0 },
+		},
+	})
 end, { desc = "Format visual selection with Conform" })
